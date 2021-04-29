9be2846e95faf2b6fec7b952c411496d55bde01c
Superoleg7 <81111991+Superoleg7@users.noreply.github.com>
2021-04-29 14:48:02 +0300

    Add files via upload

---
+++ src/Laba_2_Alphabet.java
@@ -0,0 +1,11 @@
+public class Laba_2_Alphabet {
+    public static void main(String[] args) {
+
+        for(char i = 'a';i<='z';i++)
+        {
+            System.out.println(i);
+        }
+
+    }
+}
+
