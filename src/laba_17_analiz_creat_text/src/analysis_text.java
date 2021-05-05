6204dbb8c2af94890f8ca0299b4311b54e5adfe3
Superoleg7 <81111991+Superoleg7@users.noreply.github.com>
2021-05-05 21:46:24 +0300

    Add files via upload

---
+++ src/laba_17_analiz_creat_text/src/analysis_text.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class analysis_text {
+    public class AnalizTexta {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Введите строку: ");
+            String stroka = sc.nextLine();
+            int dl = stroka.length();
+            int bykva = 0;
+            int slovo = 0;
+            System.out.println(stroka);
+            if (stroka == "") {
+                System.out.println("Пустая строка!");
+            } else {
+                for (int i = 0; i < (dl - 1); i++) {
+
+                    if (addword(stroka.charAt(i)) == 0) {
+                        bykva++;
+                        if (i + 1 == (dl - 1)) {
+                            slovo++;
+                            break;
+                        } else if (addword(stroka.charAt(i + 1)) == 1) {
+                            slovo++;
+                        }
+                    } else {
+                        if (i + 1 == (dl - 1)) {
+
+                            if (addword(stroka.charAt(dl - 1)) == 0) {
+                                slovo++;
+                            }
+                        }
+                    }
+                }
+                if (addword(stroka.charAt(dl - 1)) == 0) {
+                    bykva++;
+                }
+                System.out.println("Количество букв = " + bykva);
+                System.out.println("Количество слов = " + slovo);
+            }
+        }
+    }
+}
